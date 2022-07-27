0d6a09d83290aca1f8a25b792c384d98af8c9045
suarezfer1712 <suarezfer1712@gmail.com>
2022-07-27 22:18:22 +0100

    added some scss to the location page

--- full-stack-project-react/src/components/Nav/Nav.jsx
+++ full-stack-project-react/src/components/Nav/Nav.jsx
@@ -4,11 +4,11 @@ import SearchWidget from '../Search/SearchWidget';
 const Nav = () => {
   return (
     <div className="nav">
-      <Link className="nav-menu__item" to="/">
+      <Link className="nav__menu-item" to="/">
         Home
       </Link>
 
-      <Link className="nav-menu__item" to="/places">
+      <Link className="nav__menu-item" to="/places">
         Places to visit
       </Link>
 
